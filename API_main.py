97ad81fe0fb8919a065edb82154c56a93b79c240
Alex Zack <89016122+phenomen21@users.noreply.github.com>
2022-06-19 10:32:52 +0700

    Update API_main.py

--- API_main.py
+++ API_main.py
@@ -29,40 +29,40 @@ dict1 = {'rooms':1,
          'district':0}
 '''
 
-class Predict(Resource):
-    def xgb_predict(self, input_dict):
-        input_arr = [input_dict['rooms'], 
-                     input_dict['area_tot'],
-                     input_dict['area_kitchen'],
-                     input_dict['floor'],
-                     input_dict['floor_tot'],
-                     input_dict['district']]
-        input_processed = col_transf.transform(np.array(input_arr).reshape(1,-1))
-        output_dict = input_dict.copy()
-        output_dict['price'] = int(round(xgb_trained.predict(xgb.DMatrix(input_processed)).item(),0))
-        return output_dict
-    
-    def get(self):
-        print('start')
-        parser = reqparse.RequestParser()  # initialize
-        parser.add_argument('rooms', required=True, location='args', type=int,dest='rooms')  # add args
-        parser.add_argument('area_tot', required=True, type=float, dest='area_tot')
-        parser.add_argument('area_kitchen', required=True, type=float, dest='area_kitchen')
-        parser.add_argument('floor', required=True, type=int, dest='floor')
-        parser.add_argument('floor_tot', required=True, type=int, dest='floor_tot')
-        parser.add_argument('district', required=True, type=int, dest='district')
-        args = dict(parser.parse_args())  # parse arguments to dictionary
-        try:
-            output = self.xgb_predict(args)
-            status = 200
-        except ValueError:
-            output = {'data':'Bad request: ValueError'}
-            status = 400
-        return output, status  # return data and status
+
+def xgb_predict(input_dict):
+    input_arr = [input_dict['rooms'], 
+                 input_dict['area_tot'],
+                 input_dict['area_kitchen'],
+                 input_dict['floor'],
+                 input_dict['floor_tot'],
+                 input_dict['district']]
+    input_processed = col_transf.transform(np.array(input_arr).reshape(1,-1))
+    output_dict = input_dict.copy()
+    output_dict['price'] = int(round(xgb_trained.predict(xgb.DMatrix(input_processed)).item(),0))
+    return output_dict
 
 app = Flask(__name__)
-api = Api(app)
-api.add_resource(Predict(), '/predict')  # '/predict' is our entry point
+
+@app.route('/predict', methods=['GET'])
+def process_request():
+    print('start')
+    parser = reqparse.RequestParser()  # initialize
+    parser.add_argument('rooms', required=True, location='args', type=int,dest='rooms')  # add args
+    parser.add_argument('area_tot', required=True, type=float, dest='area_tot')
+    parser.add_argument('area_kitchen', required=True, type=float, dest='area_kitchen')
+    parser.add_argument('floor', required=True, type=int, dest='floor')
+    parser.add_argument('floor_tot', required=True, type=int, dest='floor_tot')
+    parser.add_argument('district', required=True, type=int, dest='district')
+    args = dict(parser.parse_args())  # parse arguments to dictionary
+    try:
+        output = self.xgb_predict(args)
+        status = 200
+    except ValueError:
+        output = {'data':'Bad request: ValueError'}
+        status = 400
+    return output, status  # return data and status
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
